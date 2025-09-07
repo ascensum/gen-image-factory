0e1093c7d847905377d2fba47f5ee287397cbf04
Vjatseslav Jertsalov <vjatseslav.jertsalov@gmail.com>
2025-09-07 20:46:03 +0300

    chore(types): restore shared ProcessingSettings type (processing.d.ts)

---
+++ src/types/processing.d.ts
@@ -0,0 +1,15 @@
+export interface ProcessingSettings {
+  imageEnhancement: boolean;
+  sharpening: number;
+  saturation: number;
+  imageConvert: boolean;
+  convertToJpg: boolean;
+  jpgQuality: number;
+  pngQuality: number;
+  removeBg: boolean;
+  removeBgSize: string;
+  trimTransparentBackground: boolean;
+  jpgBackground: string;
+}
+
+
